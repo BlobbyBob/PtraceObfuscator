248097171cdba04c4e29334155b0bdcca0693beb
Ben Swierzy <swierzy@uni-bonn.de>
2020-12-07 22:27:38 +0100

    Structured runtime

--- runtime.go
+++ runtime.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/BlobbyBob/NOPfuscator/common"
-	"io/ioutil"
+	. "github.com/BlobbyBob/NOPfuscator/runtime"
 	"os"
 	"os/signal"
 	"syscall"
@@ -12,75 +10,51 @@ import (
 
 func main() {
 	if len(os.Args) < 3 {
-		panic("Arguments missing.\n Usage: runtime myprogram.obf myprogram.meta")
+		panic("Arguments missing.\n Usage: runtime myprogram ...args")
 	}
 
-	obfBinaryFilename := os.Args[1]
-	metadataFilename := os.Args[2]
+	obfBinaryFilename := os.Args[1] // + ".obf"
+	metadataFilename := os.Args[1] + ".obf.meta"
 
-	metadataFile, err := ioutil.ReadFile(metadataFilename)
-	if err != nil {
+	runtime := NewRuntime()
+	if err := runtime.ReadMetadata(metadataFilename); err != nil {
 		panic(err)
 	}
-
-	var metadataRaw []common.ExportObfuscatedInstruction
-	if err := json.Unmarshal(metadataFile, &metadataRaw); err != nil {
-		panic(err)
-	}
-
-	metadata, err := common.ImportObfuscatedInstructions(metadataRaw)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Number of obfuscated instructions:", len(metadata))
 
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals)
 	go func() {
 		for {
 			s := <-signals
-			fmt.Println("Got signal", s)
+			if s == syscall.SIGINT {
+				fmt.Println("Interrupt. Terminating...")
+				os.Exit(0)
+			} else if s == syscall.SIGCHLD {
+				fmt.Println("Child Exited. Terminating...")
+				//os.Exit(0)
+			} else {
+				fmt.Println("Received signal:", s)
+			}
 		}
 	}()
 
-
-	p, err := os.StartProcess(obfBinaryFilename, os.Args[2:], &os.ProcAttr{
-		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
-		Sys: &syscall.SysProcAttr{
-			Ptrace: true,
-			Pdeathsig: syscall.SIGCHLD,
-		},
-	})
-	if err != nil {
+	fmt.Printf("Starting program '%s' with args %v\n", obfBinaryFilename, os.Args[1:])
+	if err := runtime.StartAndBreakProgram(obfBinaryFilename, os.Args[1:]); err != nil {
 		panic(err)
 	}
 
-	processState, err := p.Wait()
-	if err != nil {
+	stop := make(chan interface{})
+
+	go func() {
+		<-runtime.Event
+		stop <- true
+	}()
+
+	if err := runtime.Continue(); err != nil {
 		panic(err)
 	}
 
-	waitStatus, _ := processState.Sys().(syscall.WaitStatus)
-	fmt.Println("Exited?", waitStatus.Exited())
-
-	var reg syscall.PtraceRegs
-	if err := syscall.PtraceGetRegs(p.Pid, &reg); err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("RIP: 0x%016x\n", reg.Rip)
-
-	fmt.Println("Running program to completion")
-
-	if err := syscall.PtraceCont(p.Pid, 0); err != nil {
-		panic(err)
-	}
-
-	if _, err = p.Wait(); err != nil {
-		panic(err)
-	}
-
+	<-stop
 	fmt.Println("Done")
 
 }
---
+++ runtime/instructions.go
@@ -0,0 +1,82 @@
+package runtime
+
+func (*Runtime) jump() {
+
+}
+
+func (*Runtime) jumpAbove() {
+
+}
+
+func (*Runtime) jumpAboveOrEqual() {
+
+}
+
+func (*Runtime) jumpBelow() {
+
+}
+
+func (*Runtime) jumpBelowOrEqual() {
+
+}
+
+func (*Runtime) jumpGreater() {
+
+}
+
+func (*Runtime) jumpGreaterOrEqual() {
+
+}
+
+func (*Runtime) jumpLower() {
+
+}
+
+func (*Runtime) jumpLowerOrEqual() {
+
+}
+
+func (*Runtime) jumpEqual() {
+
+}
+
+func (*Runtime) jumpNotEqual() {
+
+}
+
+func (*Runtime) jumpOdd() {
+
+}
+
+func (*Runtime) jumpNotOdd() {
+
+}
+
+func (*Runtime) jumpParity() {
+
+}
+
+func (*Runtime) jumpNotParity() {
+
+}
+
+func (*Runtime) jumpSign() {
+
+}
+
+func (*Runtime) jumpNotSign() {
+
+}
+
+func (*Runtime) jumpCxZero() {
+
+}
+
+func (*Runtime) jumpEcxZero() {
+
+}
+
+func (*Runtime) jumpRcxZero() {
+
+}
+//x86asm.JS:
